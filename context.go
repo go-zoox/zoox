d0cdab758763ea266c32e96ed202d476cd355fc7
whwewant <tobewhatwewant@outlook.com>
2022-06-17 00:23:06 +0800

    chore: context more

--- context.go
+++ context.go
@@ -2,8 +2,15 @@ package zoox
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
+	"mime/multipart"
 	"net/http"
+	"os"
+	"time"
+
+	"github.com/go-yaml/yaml"
 )
 
 // Context is the request context
@@ -15,7 +22,7 @@ type Context struct {
 	Method string
 	Path   string
 	//
-	Params map[string]string
+	params map[string]string
 	// response
 	StatusCode int
 	// middleware
@@ -65,7 +72,7 @@ func (ctx *Context) Query(key string) string {
 
 // Param returns the named URL parameter value if it exists.
 func (ctx *Context) Param(key string) string {
-	if value, ok := ctx.Params[key]; ok {
+	if value, ok := ctx.params[key]; ok {
 		return value
 	}
 
@@ -86,6 +93,21 @@ func (ctx *Context) Status(status int) {
 // SetHeader sets a header in the response.
 func (ctx *Context) SetHeader(key string, value string) {
 	ctx.Writer.Header().Set(key, value)
+}
+
+// SetCookie sets a cookie with the given name and value.
+func (ctx *Context) SetCookie(name string, value string, maxAge time.Duration) {
+	expires := time.Now().Add(maxAge)
+
+	ctx.SetHeader(
+		"Set-Cookie",
+		fmt.Sprintf("%s=%s; path=/; expires=%s; httponly", name, value, expires),
+	)
+}
+
+// BasicAuth returns the user/password pair for Basic Authentication.
+func (ctx *Context) BasicAuth() (string, string, bool) {
+	return ctx.Request.BasicAuth()
 }
 
 // Write writes the data to the connection.
@@ -155,4 +177,186 @@ func (ctx *Context) Redirect(url string, status ...int) {
 
 	ctx.Status(code)
 	ctx.SetHeader("location", url)
+}
+
+// Header gets the header value by key.
+func (ctx *Context) Header(key string) string {
+	return ctx.Request.Header.Get(key)
+}
+
+// Headers gets all headers.
+func (ctx *Context) Headers() map[string]string {
+	headers := map[string]string{}
+
+	for key, values := range ctx.Request.Header {
+		headers[key] = values[0]
+	}
+
+	return headers
+}
+
+// Queries gets all queries.
+func (ctx *Context) Queries() map[string]string {
+	queries := map[string]string{}
+
+	for key, values := range ctx.Request.URL.Query() {
+		queries[key] = values[0]
+	}
+
+	return queries
+}
+
+// Forms gets all forms.
+func (ctx *Context) Forms() map[string]string {
+	forms := map[string]string{}
+
+	if err := ctx.Request.ParseForm(); err != nil {
+		return forms
+	}
+
+	for key, values := range ctx.Request.Form {
+		forms[key] = values[0]
+	}
+
+	return forms
+}
+
+// Params gets all params.
+func (ctx *Context) Params() map[string]string {
+	return ctx.params
+}
+
+// Bodies gets all bodies.
+func (ctx *Context) Bodies() map[string]any {
+	var bodies map[string]any
+
+	if bytes, err := io.ReadAll(ctx.Request.Body); err == nil {
+		if err := json.Unmarshal(bytes, &bodies); err == nil {
+			return bodies
+		}
+	}
+
+	return nil
+}
+
+// Cookies gets all cookies.
+func (ctx *Context) Cookies() map[string]string {
+	cookies := map[string]string{}
+
+	for _, cookie := range ctx.Request.Cookies() {
+		cookies[cookie.Name] = cookie.Value
+	}
+
+	return cookies
+}
+
+// Cookie gets the cookie value by key.
+func (ctx *Context) Cookie(key string) string {
+	cookie, err := ctx.Request.Cookie(key)
+	if err != nil {
+		return ""
+	}
+
+	return cookie.Value
+}
+
+// Files gets all files.
+func (ctx *Context) Files() map[string]*multipart.FileHeader {
+	if err := ctx.Request.ParseMultipartForm(32 << 20); err != nil {
+		return nil
+	}
+
+	if ctx.Request.MultipartForm == nil || ctx.Request.MultipartForm.File == nil {
+		return nil
+	}
+
+	files := map[string]*multipart.FileHeader{}
+	for key, file := range ctx.Request.MultipartForm.File {
+		if len(file) > 0 {
+			files[key] = file[0]
+		}
+	}
+
+	return files
+}
+
+// File gets the file by key.
+func (ctx *Context) File(key string) multipart.File {
+	if file, _, err := ctx.Request.FormFile(key); err == nil {
+		return file
+	} else {
+		return nil
+	}
+}
+
+// Stream get the body stream.
+func (ctx *Context) Stream() io.ReadCloser {
+	return ctx.Request.Body
+}
+
+// BindJSON binds the request body into the given struct.
+func (ctx *Context) BindJSON(obj interface{}) error {
+	if ctx.Request.Body == nil {
+		return errors.New("invalid request")
+	}
+
+	return json.NewDecoder(ctx.Request.Body).Decode(obj)
+}
+
+// BindYAML binds the request body into the given struct.
+func (ctx *Context) BindYAML(obj interface{}) error {
+	if ctx.Request.Body == nil {
+		return errors.New("invalid request")
+	}
+
+	return yaml.NewDecoder(ctx.Request.Body).Decode(obj)
+}
+
+// BindForm binds the query into the given struct.
+func (ctx *Context) BindForm(obj interface{}) error {
+	forms := ctx.Forms()
+	jf, _ := json.Marshal(forms)
+	return json.Unmarshal(jf, obj)
+}
+
+// BindParams binds the params into the given struct.
+func (ctx *Context) BindParams(obj interface{}) error {
+	params := ctx.Params()
+	jf, _ := json.Marshal(params)
+	return json.Unmarshal(jf, obj)
+}
+
+// BindHeader binds the header into the given struct.
+func (ctx *Context) BindHeader(obj interface{}) error {
+	headers := ctx.Headers()
+	jf, _ := json.Marshal(headers)
+	return json.Unmarshal(jf, obj)
+}
+
+// BindQuery binds the query into the given struct.
+func (ctx *Context) BindQuery(obj interface{}) error {
+	queries := ctx.Queries()
+	jf, _ := json.Marshal(queries)
+	return json.Unmarshal(jf, obj)
+}
+
+// SaveFile saves the file to the given path.
+func (ctx *Context) SaveFile(key, path string) error {
+	src, _, err := ctx.Request.FormFile(key)
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	dst, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	if _, err := io.Copy(dst, src); err != nil {
+		return err
+	}
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -2,9 +2,14 @@ module github.com/go-zoox/zoox
 
 go 1.18
 
-require github.com/go-zoox/logger v1.1.3
+require (
+	github.com/go-yaml/yaml v2.1.0+incompatible
+	github.com/go-zoox/logger v1.1.3
+)
 
 require (
 	github.com/go-zoox/chalk v1.0.1 // indirect
 	github.com/go-zoox/datetime v1.0.4 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
