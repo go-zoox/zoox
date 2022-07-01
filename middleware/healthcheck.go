5bb06d2e01f8320ba84164b7bd8427280cb59e35
whatwewant <tobewhatwewant@outlook.com>
2022-07-01 10:41:42 +0800

    fix: middleware healthcehck

--- middleware/healthcheck.go
+++ middleware/healthcheck.go
@@ -4,6 +4,11 @@ import "github.com/go-zoox/zoox"
 
 func HealthCheck() zoox.HandlerFunc {
 	return func(ctx *zoox.Context) {
-		ctx.String(200, "OK")
+		if ctx.Path == "/health" {
+			ctx.String(200, "OK")
+			return
+		}
+
+		ctx.Next()
 	}
 }
