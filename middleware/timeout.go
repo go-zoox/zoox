2c756656597cbaad22b91550fa580863cd52c1d6
whatwewant <tobewhatwewant@outlook.com>
2022-07-01 09:16:48 +0800

    chore: add timeout middleware

---
+++ middleware/timeout.go
@@ -0,0 +1,25 @@
+package middleware
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/go-zoox/zoox"
+)
+
+func Timeout(timeout time.Duration) zoox.HandlerFunc {
+	return func(ctx *zoox.Context) {
+		c, cancel := context.WithTimeout(ctx.Request.Context(), timeout)
+		defer func() {
+			cancel()
+			if c.Err() == context.DeadlineExceeded {
+				ctx.Status(http.StatusGatewayTimeout)
+			}
+		}()
+
+		ctx.Request = ctx.Request.WithContext(c)
+
+		ctx.Next()
+	}
+}
