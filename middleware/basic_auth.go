bb1d0977565182e7c2ce2bbaf167083c1d590588
whatwewant <tobewhatwewant@outlook.com>
2022-07-01 09:05:14 +0800

    chore: add basic auth middleware

---
+++ middleware/basic_auth.go
@@ -0,0 +1,26 @@
+package middleware
+
+import (
+	"crypto/subtle"
+
+	"github.com/go-zoox/zoox"
+)
+
+func BasicAuth(realm string, credentials map[string]string) zoox.HandlerFunc {
+	return func(ctx *zoox.Context) {
+		user, pass, ok := ctx.Request.BasicAuth()
+		if !ok {
+			ctx.Set("WWW-Authenticate", `Basic realm="`+realm+`"`)
+			ctx.Status(401)
+			return
+		}
+
+		credPass, credUserOk := credentials[user]
+		if !credUserOk || subtle.ConstantTimeCompare([]byte(pass), []byte(credPass)) != 1 {
+			ctx.Status(401)
+			return
+		}
+
+		ctx.Next()
+	}
+}
