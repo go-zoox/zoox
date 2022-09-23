e4840e98ec21eb39356ea685c62ebac26ec09b00
whatwewant <tobewhatwewant@outlook.com>
2022-09-23 13:00:41 +0800

    fix(middleware): proxy + rewrite with new config for rewrites

--- middleware/proxy.go
+++ middleware/proxy.go
@@ -4,28 +4,44 @@ import (
 	"regexp"
 
 	"github.com/go-zoox/proxy"
+	"github.com/go-zoox/proxy/utils/rewriter"
 	"github.com/go-zoox/zoox"
 )
 
 // ProxyConfig is the config of proxy middlewares
 type ProxyConfig struct {
-	Rewrites map[string]ProxyRewrite
+	// Rewrites map[string]ProxyRewrite
+	Rewrites ProxyGroupRewrites
+}
+
+// ProxyGroupRewrites is a list of rewrite rules
+type ProxyGroupRewrites []ProxyGroupRewrite
+
+// ProxyGroupRewrite is a group of proxy rewrites
+type ProxyGroupRewrite struct {
+	Name    string
+	RegExp  string
+	Rewrite ProxyRewrite
 }
 
 // ProxyRewrite ...
 type ProxyRewrite struct {
 	Target   string
-	Rewrites map[string]string
+	Rewrites ProxyRewriteRules // map[string]string
 }
+
+// ProxyRewriteRules ...
+type ProxyRewriteRules = rewriter.Rewriters
 
 // Proxy is a middleware that authenticates via Basic Auth.
 func Proxy(cfg *ProxyConfig) zoox.Middleware {
 	return func(ctx *zoox.Context) {
-		for key, value := range cfg.Rewrites {
-			if matched, err := regexp.MatchString(key, ctx.Path); err == nil && matched {
+		for _, group := range cfg.Rewrites {
+			if matched, err := regexp.MatchString(group.RegExp, ctx.Path); err == nil && matched {
 				// @BUG: this is not working
-				p := proxy.NewSingleTarget(value.Target, &proxy.SingleTargetConfig{
-					Rewrites: value.Rewrites,
+				p := proxy.NewSingleTarget(group.Rewrite.Target, &proxy.SingleTargetConfig{
+					Rewrites: group.Rewrite.Rewrites,
+					// ChangeOrigin: true,
 				})
 
 				p.ServeHTTP(ctx.Writer, ctx.Request)
--- middleware/rewrite.go
+++ middleware/rewrite.go
@@ -7,26 +7,20 @@ import (
 
 // RewriteConfig is the configuration for the Rewrite middleware.
 type RewriteConfig struct {
-	Rewrites map[string]string
+	Rewrites rewriter.Rewriters
 }
 
 // Rewrite is a middleware that rewrites the request path.
 func Rewrite(cfg ...*RewriteConfig) zoox.Middleware {
-	cfgX := &RewriteConfig{}
-	if len(cfg) > 0 {
-		cfgX = cfg[0]
-	}
-
-	rewriters := rewriter.Rewriters{}
-	for k, v := range cfgX.Rewrites {
-		rewriters = append(rewriters, &rewriter.Rewriter{
-			From: k,
-			To:   v,
-		})
+	rewrites := rewriter.Rewriters{}
+	if len(cfg) > 0 && cfg[0] != nil {
+		if cfg[0].Rewrites != nil {
+			rewrites = cfg[0].Rewrites
+		}
 	}
 
 	return func(ctx *zoox.Context) {
-		newPath := rewriters.Rewrite(ctx.Path)
+		newPath := rewrites.Rewrite(ctx.Path)
 		if newPath != ctx.Path {
 			ctx.Request.URL.Path = newPath
 			ctx.Path = newPath
