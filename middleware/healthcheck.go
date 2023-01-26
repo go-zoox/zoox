d2aee9a503d11a908465ac21e32a0c6d6f2f3076
whatwewant <tobewhatwewant@outlook.com>
2023-01-26 17:52:34 +0800

    chore(middleware): healthcheck allow custom health check path

--- middleware/healthcheck.go
+++ middleware/healthcheck.go
@@ -3,9 +3,14 @@ package middleware
 import "github.com/go-zoox/zoox"
 
 // HealthCheck is a middleware that checks the health of the application.
-func HealthCheck() zoox.Middleware {
+func HealthCheck(path ...string) zoox.Middleware {
+	pathX := "/health"
+	if len(path) > 0 && path[0] != "" {
+		pathX = path[0]
+	}
+
 	return func(ctx *zoox.Context) {
-		if ctx.Path == "/health" {
+		if ctx.Path == pathX {
 			ctx.String(200, "OK")
 			return
 		}
