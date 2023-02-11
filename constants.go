3779e0ce61fbc7a786c215a349129d1bf3fc0919
whatwewant <tobewhatwewant@outlook.com>
2023-02-11 12:01:25 +0800

    test: fix lint

--- constants.go
+++ constants.go
@@ -29,5 +29,8 @@ func DefaultGroup(prefix string, fn func(r *RouterGroup)) {
 	DefaultGroupsFns[prefix] = fn
 }
 
+// DefaultSecretKey uses for session encryption and decryption.
 var DefaultSecretKey = random.String(16)
+
+// DefaultSessionMaxAge is the default session max age.
 var DefaultSessionMaxAge = 1 * 24 * time.Hour
