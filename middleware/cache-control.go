b9c6a12798e7e4a64ba000d0bf9c4a3a1987f47a
whatwewant <tobewhatwewant@outlook.com>
2023-03-04 15:54:42 +0800

    chore(middleware): add cache-control

---
+++ middleware/cache-control.go
@@ -0,0 +1,53 @@
+package middleware
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/go-zoox/core-utils/regexp"
+	"github.com/go-zoox/headers"
+	"github.com/go-zoox/zoox"
+)
+
+// CacheControlConfig ...
+type CacheControlConfig struct {
+	Paths  []string
+	MaxAge time.Duration
+	//
+	Items []*CacheControlItem
+}
+
+type CacheControlItem struct {
+	Path   regexp.RegExp
+	MaxAge time.Duration
+}
+
+// CacheControl is a middleware that adds a "Cache-Control" header to the request.
+func CacheControl(cfg *CacheControlConfig) zoox.Middleware {
+	for _, path := range cfg.Paths {
+		re, err := regexp.New(path)
+		if err != nil {
+			panic(err)
+		}
+
+		cfg.Items = append(cfg.Items, &CacheControlItem{
+			Path:   re,
+			MaxAge: cfg.MaxAge,
+		})
+	}
+
+	return func(ctx *zoox.Context) {
+		if cfg.Items != nil {
+			for _, item := range cfg.Items {
+				if item.Path.Match(ctx.Path) {
+					maxAge := cfg.MaxAge / time.Second
+					// ctx.Logger.Infof("[middleware][cache-control] hit path: %s, max-age: %d", ctx.Path, maxAge)
+					ctx.Set(headers.CacheControl, fmt.Sprintf("public, max-age=%d", maxAge))
+					break
+				}
+			}
+		}
+
+		ctx.Next()
+	}
+}
