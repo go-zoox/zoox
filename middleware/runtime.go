df4b9fb4eb26641f3292392493895968a0fda859
Zero <tobewhatwewant@gmail.com>
2023-06-27 09:58:01 +0800

    chore: add runtime info

--- middleware/runtime.go
+++ middleware/runtime.go
@@ -6,10 +6,12 @@ import (
 	"github.com/go-zoox/zoox"
 )
 
+const DefaultRuntimePath = "/_/runtime"
+
 // Runtime ...
 func Runtime() zoox.Middleware {
 	return func(ctx *zoox.Context) {
-		if ctx.Path == "/_/runtime" {
+		if ctx.Path == DefaultRuntimePath {
 			info := ctx.App.Runtime().Info()
 			info.Version = zoox.Version
 
