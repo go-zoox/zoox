497a3c2baff219a8d068e78ce97516f6ad7b7137
Zero <tobewhatwewant@gmail.com>
2023-06-27 09:54:25 +0800

    chore: add WrapF

--- wrap.go
+++ wrap.go
@@ -10,3 +10,10 @@ func WrapH(handler http.Handler) HandlerFunc {
 		handler.ServeHTTP(ctx.Writer, ctx.Request)
 	}
 }
+
+// WrapF wraps a http.HandlerFunc to a HandlerFunc
+func WrapF(handler http.HandlerFunc) HandlerFunc {
+	return func(ctx *Context) {
+		handler(ctx.Writer, ctx.Request)
+	}
+}
