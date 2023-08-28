2311ce639288df4c879f6a9a91414d443a176d1b
whatwewant <tobewhatwewant@outlook.com>
2023-08-28 11:31:01 +0800

    fix(devtools): add version

--- cmd/zoox/main.go
+++ cmd/zoox/main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"github.com/go-zoox/cli"
+	"github.com/go-zoox/zoox"
 	"github.com/go-zoox/zoox/cmd/zoox/commands"
 )
 
 func main() {
 	app := cli.NewMultipleProgram(&cli.MultipleProgramConfig{
-		Name:  "zoox",
-		Usage: "zoox devtools",
+		Name:    "zoox",
+		Usage:   "zoox devtools",
+		Version: zoox.Version,
 	})
 
 	commands.Dev(app)
