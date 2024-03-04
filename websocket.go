7f2988e800de4de65f817bfdf01cba4cee7aa843
Zero <tobewhatwewant@outlook.com>
2024-03-04 21:14:09 +0800

    feat: allow websocket and http in the same route

--- websocket.go
+++ websocket.go
@@ -2,9 +2,16 @@ package zoox
 
 import (
 	"net/http"
+	"strings"
+
+	"github.com/go-zoox/headers"
+	"github.com/go-zoox/logger"
 
 	websocket "github.com/go-zoox/websocket/server"
 )
+
+const headerConnectionValueUpgrade = "upgrade"
+const headerUpgradeValueWebSocket = "websocket"
 
 // WebSocketOption ...
 type WebSocketOption struct {
@@ -27,13 +34,46 @@ func (g *RouterGroup) WebSocket(path string, opts ...func(opt *WebSocketOption))
 		opt.Server = server
 	}
 
-	handleFunc := append(opt.Middlewares, func(ctx *Context) {
-		ctx.Status(200)
+	// handleFunc := append(opt.Middlewares, func(ctx *Context) {
+	// 	ctx.Status(200)
+
+	// 	opt.Server.ServeHTTP(ctx.Writer, ctx.Request)
+	// })
+
+	// g.addRoute(http.MethodGet, path, handleFunc...)
+
+	logger.Info("[router] register: WS %s", path)
+	g.Use(func(ctx *Context) {
+		// ignore@1: method != get
+		if ctx.Method != http.MethodGet {
+			ctx.Next()
+			return
+		}
+
+		// ignore@2: connection != Upgrade
+		connection := ctx.Header().Get(headers.Connection)
+		if connection == "" {
+			ctx.Next()
+			return
+		}
+		if strings.ToLower(connection) != headerConnectionValueUpgrade {
+			ctx.Next()
+			return
+		}
+
+		// ignore@3: upgrade != websocket
+		upgrade := ctx.Header().Get(headers.Upgrade)
+		if upgrade == "" {
+			ctx.Next()
+			return
+		}
+		if strings.ToLower(upgrade) != headerUpgradeValueWebSocket {
+			ctx.Next()
+			return
+		}
 
 		opt.Server.ServeHTTP(ctx.Writer, ctx.Request)
 	})
-
-	g.addRoute(http.MethodGet, path, handleFunc...)
 
 	return opt.Server, nil
 }
